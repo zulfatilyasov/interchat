6c7a8bc20b6e50e3cbb7973d71c17dacf6ff38d6
Zulfat Ilyasov <mail@zulfat.net>
2014-12-17 20:03:59 +0300

    mongo connection

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 var express = require('express');
 var app = express();
 
+var mongo = require('mongodb').MongoClient;
+var uri = "mongodb://zulfat:518009@ds043927.mongolab.com:43927/inter-chat";
+
+mongo.connect(uri, function(err, db) {
+  if(err){
+    console.log("error: unable to connect to db") 
+  }
+  
+  db.collection('main').findOne({}, function(err, item) {
+    console.log(item);
+  });
+});
+
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/'));
 
